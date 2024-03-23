327650e9db97d795a5952e33a367b50ca01ec0ea
brianshrry <brian.shrry@gmail.com>
2024-03-22 20:44:31 -0400

    third commit

--- cypress/e2e/fp2methods.cy.js
+++ cypress/e2e/fp2methods.cy.js
@@ -1,4 +1,19 @@
 let token = null;
+describe("creating user account", () => {
+    it('should create a new account', () => {
+        cy.request({
+          method: 'POST',
+          url: 'https://thinking-tester-contact-list.herokuapp.com/users',
+          body: {
+            "firstName": "test",
+           "lastName": "user",
+            "email": "test321@fake321.com",
+            "password": "password"
+        }
+    })
+  })
+})
+
 describe('API Tests', () => {
   beforeEach('should log in and retrieve token', () => {
     cy.request({
